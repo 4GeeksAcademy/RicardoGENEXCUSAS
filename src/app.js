454c7f22c6f1e5e6976eb28ee3af0cb5f64cd915
Ricky <rikigibson199@gmail.com>
2025-03-26 20:16:40 +0000

    Proyecto Finalizado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,22 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
+  
+
+  let who = ['THE ALIENS', 'The bird', 'A Baby', 'Just a normal guy', 'Camarom from the island'];
+  let action = ['Eat', 'Respect', 'Understood', 'Shooted', 'Barked'];
+  let what = ['My code', 'the food', 'The Book', 'The 1965 documents', 'the wall'];
+  let when = ['When you were watching', 'In the summer time', 'Last days of november', 'out of the time', 'today'];
+  
+  function getRandomNumber(a,b) {
+    return Math.floor(Math.random() * (b - a) + a)
+  }
+
+  function getRandom(anyArray) {
+    let max = anyArray.length;
+    let min = 0;
+    let random = getRandomNumber(min, max);
+    return anyArray[random];
+  }
+  document.getElementById("AAAAA").innerHTML = getRandom(who)+ " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when);
+}
